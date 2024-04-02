82b38eb01f04f319e00e2dce0cc86633b8b629f0
ahmedbasemdev <ahmedbasem.dev@gmail.com>
2024-04-02 17:20:40 +0200

    Data Transformation

--- src/MlProject/pipeline/stage_03_data_transformation.py
+++ src/MlProject/pipeline/stage_03_data_transformation.py
@@ -1,16 +1,27 @@
 from MlProject.config.configuration import ConfigurationManager
 from MlProject.components.data_transformation import DataTransformation
 from MlProject import logger
+from pathlib import Path
+import os
 
 class DataTranformationPipeLine:
     def __init__(self) -> None:
+        logger.info(os.curdir)
         pass
 
     def main(self):
-            config = ConfigurationManager()
-            data_transformation_config = config.get_data_transformation_config()
-            data_transformation = DataTransformation(data_transformation_config)
-            data_transformation.train_test_spliting()
+            try:
+                with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                      status = f.read().split(" ")[-1]
+                if status == "True":
+                    config = ConfigurationManager()
+                    data_transformation_config = config.get_data_transformation_config()
+                    data_transformation = DataTransformation(data_transformation_config)
+                    data_transformation.train_test_spliting()
+                else:
+                     raise Exception(" Your Data Schema is not valid")
+            except Exception as e:
+                 raise e
 
 STAGE_NAME = "Data Tranformation Stage"
 
